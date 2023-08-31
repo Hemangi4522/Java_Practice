66fe84657948cbade6e438034347066e2b1b7e33
Hemangi Kamalakar Bhangale <hb4522@gmail.com>
2023-08-31 14:28:41 +0530

    Programs for practice

---
+++ DollarIntoRupee.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class DollarIntoRupee {
+
+	public static void main(String[] args) {
+		double dollar=6;
+		double rupee=82.66 * dollar;
+		
+		System.out.println(dollar +" Dollar="+ rupee + " Indian Rupees");
+		
+		
+	}
+
+}
