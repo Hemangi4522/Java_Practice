3e4378f6dc2afb913b2c39ce14d884fa84a9d326
Hemangi Kamalakar Bhangale <hb4522@gmail.com>
2023-09-01 15:44:31 +0530

    Modified

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,48 +1,48 @@
 package practice;
 
+import java.util.Random;
 import java.util.Scanner;
+import java.util.random.*;
 
 public class GuessTheNumber {
 
 	public static void main(String[] args) {
-		 Scanner sc = new Scanner(System.in);
-		 
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Enter a number between 1 to 100 : ");
-		int genNo= 50;
-		int g= 5;
+		
+		Random ran =new Random();
+		int genNo = ran.nextInt(100);
+		//System.out.println(genNo);
+		int chance = 5;
 		int guess;
-		for (int i=0; i<g; i++) {
+
+		for (int i = 0; i < chance; i++) {
 			System.out.println("Guess the Number :");
-		guess =sc.nextInt();
-		 
-		if (genNo == guess) {
-			System.out.println("Congratulations ! You guessed the number.");
-			break;
+			guess = sc.nextInt();
+
+			if (genNo == guess) {
+				System.out.println("Congratulations ! You guessed the number.");
+				break;
 			}
-		
-			else if (genNo > guess && i !=g -1) {
-				System.out.println("The number is greater than "+ guess);			
+
+			else if (genNo > guess && i != chance - 1) {
+				System.out.println("The number is greater than " + guess);
+
+			} else if (genNo < guess && i != chance - 1) {
+				System.out.println("The number is less than " + guess);
+
+			}
+			if (i == chance) {
 				
+				System.out.println("The number was " + genNo);
 			}
-			else if (genNo < guess && i !=g -1) {
-				System.out.println("The number is less than"+ guess);			
-				
+			if (guess > 100) {
+				System.out.println("Please ,Enter a number between 1 to 100!");
 			}
-		if (i== g)
-		{
-			System.out.println("you are running out of guessing chances");
-			System.out.println("The number was "+genNo);
+
 		}
-		if (guess > 100) {
-			System.out.println("Please ,Enter a number between 1 to 100!");
-		}
-		
-		
-		}
-		
-		
-		
-		
+
 	}
 
 }
